38b769ef51591070383c9023762a8feb20d464b2
Thierry Bernier <tbernier@amg-dev.fr>
2016-10-28 11:50:43 +0200

    Diamond kata

---
+++ diamond/diamond2.php
@@ -0,0 +1,34 @@
+<?php
+
+function diamond2($letter){
+	$position = position($letter);
+	$nb_rows = $nb_cols = $position*2-1;
+
+	//construct multi array for first part and fill it with spaces
+	$result = array_fill(1, $nb_rows, array_fill(1, $nb_cols, ' '));
+	
+	//fill first part
+	foreach(range('a', $letter) as $char){
+		$n_row = position($char);
+		$result[$n_row][$position - (position($char)-1)] = 
+		$result[$n_row][$position + (position($char)-1)] = 
+		$result[$nb_rows - ($n_row-1)][$position - (position($char)-1)] = 
+		$result[$nb_rows - ($n_row-1)][$position + (position($char)-1)] = $char;
+	}
+
+	//convert rows array to string
+	foreach($result as $key => $row){
+		$result[$key] = implode('', $row);
+	}
+
+	$result = implode("\n", $result);
+	return $result;
+}
+
+function position($letter){
+	return ord($letter) - 96;
+}
+
+echo diamond2('j');
+
+?>
