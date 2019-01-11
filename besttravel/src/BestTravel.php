7bab89bed5b942be0aba2bc63bce4887736117a1
Thierry Bernier <tbernier@amg-dev.fr>
2019-01-11 16:32:56 +0100

    Add test with 1 or 2 towns

--- besttravel/src/BestTravel.php
+++ besttravel/src/BestTravel.php
@@ -1,19 +1,42 @@
 <?php
 
 final class BestTravel{
-	public function choose_best_sum(int $distanceLimit, int $numberOftowns, array $listOfDistances): ?array
+	public function choose_best_sum(int $distanceLimit, int $numberOftowns, array $listOfDistances): ?int
 	{
-		if($distanceLimit < 0 || $numberOftowns < 1 || empty($listOfDistances))
+		if($distanceLimit < 0 || $numberOftowns < 1 || empty($listOfDistances) || $numberOftowns > count($listOfDistances))
 		{
 			throw new InputException();
 		}
 
-		foreach($listOfDistances as $distance)
+		foreach($listOfDistances as $key => $distance)
 		{
 			$this->checkDistance($distance);
+			if($distance > $distanceLimit)
+			{
+				unset($listOfDistances[$key]);
+			}
 		}
 
-		return [];
+		if(empty($listOfDistances))
+		{
+			return null;
+		}
+
+		if($numberOftowns === 1)
+		{
+			return max($listOfDistances);
+		}
+
+		rsort($listOfDistances);
+		$result = array_shift($listOfDistances);
+		foreach ($listOfDistances as $distance) {
+			if($result + $distance <= $distanceLimit)
+			{
+				return $result + $distance;
+			}
+		}
+
+		return array_sum($listOfDistances);
 	}
 
 	private function checkDistance(int $distance): void
--- besttravel/tests/BestTravelTest.php
+++ besttravel/tests/BestTravelTest.php
@@ -34,13 +34,68 @@ final class BestTravelTest extends TestCase
         yield [[10, 15, -1, 20]];
     }
 
-        /**
+    /**
     * @dataProvider provideInputInvalidDistances
     * @expectedException InputException
     */
-    public function testIfThirdParameterIsInvalidThenRisesException($input) :void
+    public function testIfThirdParameterIsInvalidThenRaisesException($input) :void
     {
     	$BestTravel = new BestTravel();
         $output = $BestTravel->choose_best_sum(100, 3, $input);
+    }
+
+    public function provideInputInvalidInputs()
+    {
+        yield [4, [5, 46, 45]];
+        yield [5, [1]];
+    }
+
+    /**
+    * @dataProvider provideInputInvalidInputs
+    * @expectedException InputException
+    */
+    public function testIfNumberOfTownsIsLessThanNumberOfDistanceRaisesException($numberOfTowns, $listOfDistances)
+    {
+        $BestTravel = new BestTravel();
+        $output = $BestTravel->choose_best_sum(100, $numberOfTowns, $listOfDistances);
+    }
+
+    public function provideValidInputsWithOneTown()
+    {
+        yield [25, 1, [5], 5];
+        yield [25, 1, [6], 6];
+        yield [25, 1, [6, 20], 20];
+        yield [25, 1, [6, 20, 26], 20];
+        yield [25, 1, [26], null];
+    }
+
+    /**
+    * @dataProvider provideValidInputsWithOneTown
+    */
+    public function testWithOneTown($distanceLimit, $numberOfTowns, $listOfDistances, $expected)
+    {
+        $BestTravel = new BestTravel();
+        $output = $BestTravel->choose_best_sum($distanceLimit, $numberOfTowns, $listOfDistances);
+        $this->assertSame($expected, $output);
+    }
+
+    public function provideValidInputsWithTwoTowns()
+    {
+        yield [25, 2, [5, 15], 20];
+        yield [25, 2, [5, 15, 16], 21];
+        yield [25, 2, [16, 5, 15], 21];
+        yield [25, 2, [20, 2, 9], 22];
+        //todo
+        //yield [25, 2, [20, 2, 9, 8, 25], 22];
+    }
+
+    /**
+    * @dataProvider provideValidInputsWithTwoTowns
+    */
+    public function testWithTwoTowns($distanceLimit, $numberOfTowns, $listOfDistances, $expected)
+    {
+        $BestTravel = new BestTravel();
+        $output = $BestTravel->choose_best_sum($distanceLimit, $numberOfTowns, $listOfDistances);
+        $this->assertSame($expected, $output);
     }
 }
